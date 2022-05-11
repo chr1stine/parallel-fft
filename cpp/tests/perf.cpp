442cdde9ab8cfb7662979a39c2b4abddfbf049f4
chris <kristina.beidina@gmail.com>
2022-05-11 23:43:20 +0300

    edit tests for cpp

--- cpp/tests/perf.cpp
+++ cpp/tests/perf.cpp
@@ -4,432 +4,51 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
 
-// size 4
-static void BM_Sequential_size4(benchmark::State &state)
+static void BM_sequential(benchmark::State &state)
 {
-    CArray x = read_array("../tests_data/input1");
     for (auto _ : state)
+    {
+        int i = state.range(0);
+
+        char path[] = "../test_data/input";
+        char testN[6];
+        sprintf(testN, "%d", i + 1);
+        strcat(path, testN);
+
+        CArray x = read_array(path);
         CArray y = fft_iter(x);
+    }
 }
 
-static void BM_Parallel_1thread_size4(benchmark::State &state)
+static void BM_parallel(benchmark::State &state)
 {
-    CArray x = read_array("../../../tests_data/input1");
     for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
+    {
+        int i = state.range(0);
+
+        char path[] = "../test_data/input";
+        char testN[6];
+        sprintf(testN, "%d", i + 1);
+        strcat(path, testN);
+
+        CArray x = read_array(path);
+        CArray y = fft_iter_parallel(x, state.threads());
+    }
 }
 
-static void BM_Parallel_4threads_size4(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input1");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
+// sequential
+BENCHMARK(BM_sequential)->Arg(8)->Arg(9)->Arg(10)->Arg(11)->Arg(12)->Arg(13)->Arg(14)->Arg(15)->Threads(1);
 
-static void BM_Parallel_8threads_size4(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input1");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
+// 4 threads
+BENCHMARK(BM_parallel)->Arg(8)->Arg(9)->Arg(10)->Arg(11)->Arg(12)->Arg(13)->Arg(14)->Arg(15)->Threads(4);
 
-static void BM_Parallel_16threads_size4(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input1");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
+// 8 threads
+BENCHMARK(BM_parallel)->Arg(8)->Arg(9)->Arg(10)->Arg(11)->Arg(12)->Arg(13)->Arg(14)->Arg(15)->Threads(8);
 
-// size 8
-static void BM_Sequential_size8(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input2");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
+// 16 threads
+BENCHMARK(BM_parallel)->Arg(8)->Arg(9)->Arg(10)->Arg(11)->Arg(12)->Arg(13)->Arg(14)->Arg(15)->Threads(16);
 
-static void BM_Parallel_1thread_size8(benchmark::State &state)
-{
-    CArray x = read_array("../../../tests_data/input2");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size8(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input2");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size8(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input2");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size8(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input2");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 16
-static void BM_Sequential_size16(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input3");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
-
-static void BM_Parallel_1thread_size16(benchmark::State &state)
-{
-    CArray x = read_array("../../../tests_data/input3");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size16(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input3");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size16(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input3");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size16(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input3");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 32
-static void BM_Sequential_size32(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input4");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
-
-static void BM_Parallel_1thread_size32(benchmark::State &state)
-{
-    CArray x = read_array("../../../tests_data/input4");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size32(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input4");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size32(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input4");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size32(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input4");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 64
-static void BM_Sequential_size64(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input7");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
-
-static void BM_Parallel_1thread_size64(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input7");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size64(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input7");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size64(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input7");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size64(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input7");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 128
-static void BM_Sequential_size128(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input8");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
-
-static void BM_Parallel_1thread_size128(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input8");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size128(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input8");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size128(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input8");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size128(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input8");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 256
-static void BM_Sequential_size256(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input9");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
-
-static void BM_Parallel_1thread_size256(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input9");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size256(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input9");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size256(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input9");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size256(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input9");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 512
-static void BM_Sequential_size512(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input10");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
-
-static void BM_Parallel_1thread_size512(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input10");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size512(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input10");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size512(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input10");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size512(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input10");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 1024
-static void BM_Sequential_size1024(benchmark::State &state)
-{
-    // std::cout << "HERE" << std::endl;
-    CArray x = read_array("../tests_data/input5");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
-
-static void BM_Parallel_1thread_size1024(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input5");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size1024(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input5");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size1024(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input5");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size1024(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input5");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 4096
-static void BM_Sequential_size4096(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input6");
-    for (auto _ : state)
-        CArray y = fft_iter(x);
-}
-
-static void BM_Parallel_1thread_size4096(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input6");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 1);
-}
-
-static void BM_Parallel_4threads_size4096(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input6");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 4);
-}
-
-static void BM_Parallel_8threads_size4096(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input6");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 8);
-}
-
-static void BM_Parallel_16threads_size4096(benchmark::State &state)
-{
-    CArray x = read_array("../tests_data/input6");
-    for (auto _ : state)
-        CArray y = fft_iter_parallel(x, 16);
-}
-
-// size 4
-BENCHMARK(BM_Sequential_size4);
-BENCHMARK(BM_Parallel_4threads_size4);
-BENCHMARK(BM_Parallel_8threads_size4);
-BENCHMARK(BM_Parallel_16threads_size4);
-
-// size 8
-BENCHMARK(BM_Sequential_size8);
-BENCHMARK(BM_Parallel_4threads_size8);
-BENCHMARK(BM_Parallel_8threads_size8);
-BENCHMARK(BM_Parallel_16threads_size8);
-
-// size 16
-BENCHMARK(BM_Sequential_size16);
-BENCHMARK(BM_Parallel_4threads_size16);
-BENCHMARK(BM_Parallel_8threads_size16);
-BENCHMARK(BM_Parallel_16threads_size16);
-
-// size 32
-BENCHMARK(BM_Sequential_size32);
-BENCHMARK(BM_Parallel_4threads_size32);
-BENCHMARK(BM_Parallel_8threads_size32);
-BENCHMARK(BM_Parallel_16threads_size32);
-
-// size 64
-BENCHMARK(BM_Sequential_size64);
-BENCHMARK(BM_Parallel_4threads_size64);
-BENCHMARK(BM_Parallel_8threads_size64);
-BENCHMARK(BM_Parallel_16threads_size64);
-
-// size 128
-BENCHMARK(BM_Sequential_size128);
-BENCHMARK(BM_Parallel_4threads_size128);
-BENCHMARK(BM_Parallel_8threads_size128);
-BENCHMARK(BM_Parallel_16threads_size128);
-
-// size 256
-BENCHMARK(BM_Sequential_size256);
-BENCHMARK(BM_Parallel_4threads_size256);
-BENCHMARK(BM_Parallel_8threads_size256);
-BENCHMARK(BM_Parallel_16threads_size256);
-
-// size 512
-BENCHMARK(BM_Sequential_size512);
-BENCHMARK(BM_Parallel_4threads_size512);
-BENCHMARK(BM_Parallel_8threads_size512);
-BENCHMARK(BM_Parallel_16threads_size512);
-
-// size 1024
-BENCHMARK(BM_Sequential_size1024);
-BENCHMARK(BM_Parallel_4threads_size1024);
-BENCHMARK(BM_Parallel_8threads_size1024);
-BENCHMARK(BM_Parallel_16threads_size1024);
-
-// size 4096
-BENCHMARK(BM_Sequential_size1024);
-BENCHMARK(BM_Parallel_4threads_size4096);
-BENCHMARK(BM_Parallel_8threads_size4096);
-BENCHMARK(BM_Parallel_16threads_size4096);
-
-// int setup(int argc, char **argv)
-// {
-//     // ::testing::InitGoogleTest(&argc, argv);
-//     x = read_array("../../../tests_data/input1");
-//     // return RUN_ALL_TESTS();
-// }
+// 32 threads
+BENCHMARK(BM_parallel)->Arg(8)->Arg(9)->Arg(10)->Arg(11)->Arg(12)->Arg(13)->Arg(14)->Arg(15)->Threads(32);
 
 BENCHMARK_MAIN();
--- cpp/tests/unit.cpp
+++ cpp/tests/unit.cpp
@@ -7,97 +7,145 @@
 
 TEST(handlesSequentialFFT, SequentialFftReturnsCorrectValueForExample1)
 {
-    CArray x = read_array("../tests_data/input1");
+    CArray x = read_array("../test_data/input1");
     CArray y = fft_iter(x);
-    CArray y_expected = read_array("../tests_data/expected_output1");
+    CArray y_expected = read_array("../test_data/expected_output1");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesSequentialFFT, SequentialFftReturnsCorrectValueForExample2)
 {
-    CArray x = read_array("../tests_data/input2");
+    CArray x = read_array("../test_data/input2");
     CArray y = fft_iter(x);
-    CArray y_expected = read_array("../tests_data/expected_output2");
+    CArray y_expected = read_array("../test_data/expected_output2");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesSequentialFFT, SequentialFftReturnsCorrectValueForExample3)
 {
-    CArray x = read_array("../tests_data/input3");
+    CArray x = read_array("../test_data/input3");
     CArray y = fft_iter(x);
-    CArray y_expected = read_array("../tests_data/expected_output3");
+    CArray y_expected = read_array("../test_data/expected_output3");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesSequentialFFT, SequentialFftReturnsCorrectValueForExample4)
 {
-    CArray x = read_array("../tests_data/input4");
+    CArray x = read_array("../test_data/input4");
     CArray y = fft_iter(x);
-    CArray y_expected = read_array("../tests_data/expected_output4");
+    CArray y_expected = read_array("../test_data/expected_output4");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesSequentialFFT, SequentialFftReturnsCorrectValueForExample7)
 {
-    CArray x = read_array("../tests_data/input7");
+    CArray x = read_array("../test_data/input7");
     CArray y = fft_iter(x);
-    CArray y_expected = read_array("../tests_data/expected_output7");
+    CArray y_expected = read_array("../test_data/expected_output7");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesSequentialFFT, SequentialFftReturnsCorrectValueForExample9)
 {
-    CArray x = read_array("../tests_data/input9");
+    CArray x = read_array("../test_data/input9");
     CArray y = fft_iter(x);
-    CArray y_expected = read_array("../tests_data/expected_output9");
+    CArray y_expected = read_array("../test_data/expected_output9");
+    EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
+}
+
+TEST(handlesSequentialFFT, ParallelFftReturnsCorrectValueForExample10)
+{
+    CArray x = read_array("../test_data/input10");
+    CArray y = fft_iter(x);
+    CArray y_expected = read_array("../test_data/expected_output10");
+    EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
+}
+
+TEST(handlesSequentialFFT, ParallelFftReturnsCorrectValueForExample12)
+{
+    CArray x = read_array("../test_data/input12");
+    CArray y = fft_iter(x);
+    CArray y_expected = read_array("../test_data/expected_output12");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample1)
 {
-    CArray x = read_array("../tests_data/input1");
+    CArray x = read_array("../test_data/input1");
     CArray y = fft_iter_parallel(x, 4);
-    CArray y_expected = read_array("../tests_data/expected_output1");
+    CArray y_expected = read_array("../test_data/expected_output1");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample2)
 {
-    CArray x = read_array("../tests_data/input2");
+    CArray x = read_array("../test_data/input2");
     CArray y = fft_iter_parallel(x, 4);
-    CArray y_expected = read_array("../tests_data/expected_output2");
+    CArray y_expected = read_array("../test_data/expected_output2");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample3)
 {
-    CArray x = read_array("../tests_data/input3");
+    CArray x = read_array("../test_data/input3");
     CArray y = fft_iter_parallel(x, 4);
-    CArray y_expected = read_array("../tests_data/expected_output3");
+    CArray y_expected = read_array("../test_data/expected_output3");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample4)
 {
-    CArray x = read_array("../tests_data/input4");
+    CArray x = read_array("../test_data/input4");
     CArray y = fft_iter_parallel(x, 4);
-    CArray y_expected = read_array("../tests_data/expected_output4");
+    CArray y_expected = read_array("../test_data/expected_output4");
+    EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
+}
+
+TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample5)
+{
+    CArray x = read_array("../test_data/input5");
+    CArray y = fft_iter_parallel(x, 4);
+    CArray y_expected = read_array("../test_data/expected_output5");
+    EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
+}
+
+TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample6)
+{
+    CArray x = read_array("../test_data/input6");
+    CArray y = fft_iter_parallel(x, 4);
+    CArray y_expected = read_array("../test_data/expected_output6");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample7)
 {
-    CArray x = read_array("../tests_data/input7");
+    CArray x = read_array("../test_data/input7");
     CArray y = fft_iter_parallel(x, 4);
-    CArray y_expected = read_array("../tests_data/expected_output7");
+    CArray y_expected = read_array("../test_data/expected_output7");
+    EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
+}
+
+TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample8)
+{
+    CArray x = read_array("../test_data/input8");
+    CArray y = fft_iter_parallel(x, 4);
+    CArray y_expected = read_array("../test_data/expected_output8");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
 TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample9)
 {
-    CArray x = read_array("../tests_data/input9");
+    CArray x = read_array("../test_data/input9");
     CArray y = fft_iter_parallel(x, 4);
-    CArray y_expected = read_array("../tests_data/expected_output9");
+    CArray y_expected = read_array("../test_data/expected_output9");
+    EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
+}
+
+TEST(handlesParallelFFT, ParallelFftReturnsCorrectValueForExample10)
+{
+    CArray x = read_array("../test_data/input10");
+    CArray y = fft_iter_parallel(x, 4);
+    CArray y_expected = read_array("../test_data/expected_output10");
     EXPECT_TRUE(y == y_expected) << comparison_failed_message(y, y_expected);
 }
 
